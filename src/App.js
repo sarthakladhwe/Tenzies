baf4984cf4db0a64ec3d2f7558e96a0765c83edf
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-03-08 16:40:31 +0530

    holdDice functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import React from 'react'
+import {nanoid} from 'nanoid'
+
 import Die from './Components/Die'
 import Button from './Components/Button'
 
 function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
+  console.table(dice)
 
   function allNewDice() {
     const randomDieNumber = []
     for(let i=0; i<10; i++) {
-      randomDieNumber.push(Math.ceil(Math.random() * 6))
+      randomDieNumber.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      })
     }
     return randomDieNumber;
   }
-
+  
   function updateDice() {
     setDice(allNewDice())
   }
 
-  const diceElements = dice.map(number => <Die value={number}/>)
+  function holdDice(id) {
+    setDice(prevDice => (
+      prevDice.map((prevDie => (
+        prevDie.id === id ? {...prevDie, isHeld: !prevDie.isHeld} : {...prevDie}
+      )))
+    ))
+  }
+
+  const diceElements = dice.map(die => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>
+  ))
+
+  console.log("Dice Elements",diceElements)
 
   return (
     <div className="App">
       <main>
         <div className="dice-container">
-          { diceElements }
+          {diceElements}
         </div>
         <Button rollDice={updateDice}/>
       </main>
--- src/Components/Die.js
+++ src/Components/Die.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-export default function Die({ value }) {
+export default function Die({ value, isHeld, holdDice }) {
     return (
-        <div className="die-face">
+        <div 
+            className="die-face" 
+            style={{backgroundColor: isHeld ? "#59E391" : "#fff"}} 
+            onClick={holdDice}
+        >
             <h2>{value}</h2>
         </div>
     )
